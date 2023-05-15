8e47c8863217945697b7df35a6f9560643ffa3de
ZionC <2522820243@qq.com>
2023-05-15 14:47:16 +0800

    20230515-2

---
+++ HyperMusic_backend/User/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from views import *
+urlpatterns = [
+    path('register/', register),
+    path('login/', login),
+    path('change_info/', change_info),
+    path('upload_music/', upload_music),
+    path('del_music/', del_music),
+    path('follow/', follow),
+    path('unfollow', unfollow),
+    path('get_follow_list', get_follow_list),
+    path('get_fan_list', get_fan_list),
+    path('get_create_music_list', get_create_music_list),
+    path('get_like_music_list', get_like_music_list),
+    path('get_like_singer_simple', get_like_singer_simple),
+    path('get_recent_listen_music_list', get_recent_listen_music_list),
+    path('get_most_listen_music_list', get_most_listen_music_list),
+]
