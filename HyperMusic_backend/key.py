e4540eaedbdfa8ffc595f38a6fb7cf0cca3c62bc
ZionC <96963890+afdafczl@users.noreply.github.com>
2023-05-26 15:35:04 +0800

    Delete key.py

--- HyperMusic_backend/key.py
+++
@@ -1,8 +0,0 @@
-db_user_name='root'
-db_user_password='zhgzL789'
-
-bucket_secret_id='AKIDLiD5vlXCqXtHboz9Oh4OiHnGkIpMuQky'
-bucket_secret_key='66yqeNuKc1Nfow2DUGpgVBiFl5HF2d3v'
-bucket_app_id='-1317300880'
-bucket_region='ap-beijing'
-bucket_access='private'
