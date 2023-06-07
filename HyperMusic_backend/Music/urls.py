30ba6b8ce891f16da154e2328ae9a63c70c46446
zh <1812643720@qq.com>
2023-06-07 19:17:52 +0800

    add get upload music

--- HyperMusic_backend/Music/urls.py
+++ HyperMusic_backend/Music/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('get_music_list_info/', get_music_list_info),
     path('get_music_info/', get_music_info),
     path('change_favorites_info/', change_favorites_info),
+    path('get_upload_music/', get_upload_music),
 ]
